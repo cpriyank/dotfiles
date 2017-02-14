0426a2e34cf322eeafd8bccf635fbfb1d7e951c4
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-02-13 18:42:48 -0800

    Gentle reminder to take a break

--- bin/bin/lookout.zsh
+++ bin/bin/lookout.zsh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 #
-# Notify to look out or away to reduce eye strain with customized notification
+# Notify to look outside to reduce eye strain with customized notification
 if [[ -s todo ]]
 then
 	var=$(<todo)
 	notify-send -u critical "{${var}}"
 else
-	notify-send -u critical "Winter is Coming!"
+	notify-send -a "Have a break" -u critical "`fortune`"
 fi
