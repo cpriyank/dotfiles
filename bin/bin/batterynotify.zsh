5cdc8972540d46bf4c6520c0224a9baa29da5948
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-05-06 13:36:57 -0700

    Battery life friendly notifications

---
+++ bin/bin/batterynotify.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/zsh -f
+# This is a simple script to notify on low battery via libnotify
+if [[ -e `which acpi` ]]
+then
+	battery_level=`acpi -b | cut -d ' ' -f 4 | grep -o '[0-9]*'`
+	charging_status=`acpi -b | cut -d ',' -f 1 | cut -d ' ' -f 3`
+	if [[ $battery_level -le 40 && $charging_status != 'Charging' ]];
+	then notify-send -u critical "Battery level is $battery_level" "Please connect the charger now"
+	elif [[ $battery_level -ge 90 && $charging_status == 'Charging' ]];
+	then notify-send -u normal "Battery level is $battery_level" "Please remove the charger now"
+	fi
+else
+	notify-send -u normal "acpi not installed"
+fi
