1edd97bf5d6849e9b9fcd310ae1fae146060c4c7
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-10-12 15:06:12 -0700

    changes in path to reflect addition of pure prompt submodule

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,16 @@
-# THEME PURE #
-set fish_function_path /Users/pika/.config/fish/functions/theme-pure $fish_function_path
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/functions/cdf.fish
+
+
+set -gx GOPATH ~/z/go
+set -gx PATH /usr/local/sbin ~/z/go/bin /snap/bin $PATH
 # Base16 Shell
-if status --is-interactive
-    eval sh $HOME/.config/fish/base16-monokai-dark.sh
-end
+# if status --is-interactive
+#     eval sh $HOME/.config/fish/base16-monokai-dark.sh
+# end
+
+# THEME PURE #
+set fish_function_path ~/.config/fish/functions/pure $fish_function_path
+
+set pure_username_color $pure_color_blue
+set pure_host_color $pure_color_blue
