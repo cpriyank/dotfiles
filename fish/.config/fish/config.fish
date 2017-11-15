6f149914c178f681040e2951391332c0685e217e
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2017-11-14 17:10:18 -0800

    reorganizing functions

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,8 @@
 set fish_function_path /Users/pika/.config/fish/functions/theme-pure $fish_function_path
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/functions/cdf.fish
+set -gx GOPATH ~/z/go
+set -gx PATH /usr/local/sbin /Users/pika/z/go/bin $PATH
 # Base16 Shell
 if status --is-interactive
     eval sh $HOME/.config/fish/base16-monokai-dark.sh
