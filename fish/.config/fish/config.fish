05ade7295f6d954da8a04edea7d759bd7a2f57bc
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2018-01-02 11:25:05 -0800

    config.fish: restructuring fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,73 +1,23 @@
-source ~/.config/fish/aliases.fish
-source ~/.config/fish/functions/cdf.fish
-set -gx GOPATH ~/z/go
-set -gx PATH /usr/local/sbin /Users/pika/z/go/bin $PATH
-# Base16 Shell
-if status --is-interactive
-    eval sh $HOME/.config/fish/base16-monokai.sh
+### Environment variables
+# Set EDITOR. Hope I won't have to resort to pico
+if command --search --quiet "nvim"
+    set -gx EDITOR nvim
+else if command --search --quiet "vim"
+    set -gx EDITOR vim
+else
+    set -gx EDITOR nano
 end
 
-# THEME PURE #
-set fish_function_path ~/.config/fish/functions/pure $fish_function_path
+### Some handy aliases
+source ~/.config/fish/aliases.fish
 
-set pure_username_color $pure_color_blue
-set pure_host_color $pure_color_blue
+### Language specific additions and paths
+source ~/.config/fish/go.fish
 
-# Readline colors
-set -g fish_color_autosuggestion 555 yellow
-set -g fish_color_command 5f87d7
-set -g fish_color_comment 808080
-set -g fish_color_cwd 87af5f
-set -g fish_color_cwd_root 5f0000
-set -g fish_color_error 870000 --bold
-set -g fish_color_escape af5f5f
-set -g fish_color_history_current 87afd7
-set -g fish_color_host 5f87af
-set -g fish_color_match d7d7d7 --background=303030
-set -g fish_color_normal normal
-set -g fish_color_operator d7d7d7
-set -g fish_color_param 5f87af
-set -g fish_color_quote d7af5f
-set -g fish_color_redirection normal
-set -g fish_color_search_match --background=purple
-set -g fish_color_status 5f0000
-set -g fish_color_user 5f875f
-set -g fish_color_valid_path --underline
+### Base16 Shell
+if status --is-interactive
+    eval sh $HOME/.config/base16-shell/scripts/base16-monokai.sh
+end
 
-set -g fish_color_dimmed 555
-set -g fish_color_separator 999
-
-# Git prompt status
-set -g __fish_git_prompt_showdirtystate 'yes'
-set -g __fish_git_prompt_showupstream auto
-set -g pure_git_untracked_dirty false
-
-# Status Chars
-#set __fish_git_prompt_char_dirtystate '*'
-set __fish_git_prompt_char_upstream_equal ''
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_dirtystate 'red'
-
-set __fish_git_prompt_color_upstream_ahead ffb90f
-set __fish_git_prompt_color_upstream_behind blue
-
-# Local prompt customization
-set -e fish_greeting
-
-
-set -g fish_pager_color_completion normal
-set -g fish_pager_color_description 555 yellow
-set -g fish_pager_color_prefix cyan
-set -g fish_pager_color_progress cyan
-
-
-# highlighting inside manpages and elsewhere
-set -gx LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
-set -gx LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
-set -gx LESS_TERMCAP_me \e'[0m'           # end mode
-set -gx LESS_TERMCAP_se \e'[0m'           # end standout-mode
-set -gx LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
-set -gx LESS_TERMCAP_ue \e'[0m'           # end underline
-set -gx LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+### Pure prompt theme
+set -gx fish_function_path $fish_function_path $HOME/.config/fish/pure
