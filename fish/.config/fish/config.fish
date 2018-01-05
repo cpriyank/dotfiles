44bd0955172a484193895d7d32df0b551027d9be
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2018-01-04 16:07:23 -0800

    some color to less output

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,10 +9,10 @@ else
 end
 
 ### Some handy aliases
-source ~/.config/fish/aliases.fish
+source $HOME/.config/fish/aliases.fish
 
 ### Language specific additions and paths
-source ~/.config/fish/go.fish
+source $HOME/.config/fish/go.fish
 
 ### Base16 Shell
 if status --is-interactive
@@ -20,4 +20,16 @@ if status --is-interactive
 end
 
 ### Pure prompt theme
-set fish_function_path $fish_function_path $HOME/.config/fish/pure
+set fish_function_path $fish_function_path $HOME/.config/fish/functions/pure
+source $HOME/.config/fish/functions/pure/fish_prompt.fish
+
+### Color 'less' output
+set -gx LESS_TERMCAP_mb \e'[01;31m'
+set -gx LESS_TERMCAP_md \e'[01;31m'
+set -gx LESS_TERMCAP_me \e'[0m'
+set -gx LESS_TERMCAP_se \e'[0m'
+set -gx LESS_TERMCAP_so \e'[01;44;33m'
+set -gx LESS_TERMCAP_ue \e'[0m'
+set -gx LESS_TERMCAP_us \e'[01;32m'
+
+set -gx FZF_DEFAULT_COMMAND rg
