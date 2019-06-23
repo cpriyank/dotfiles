4d8b0eed5e84cdc0bffdbb86d29f3a4552fba894
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2019-06-22 23:11:01 -0700

    beamer dependencies

--- init/tlmgr.sh
+++ init/tlmgr.sh
@@ -1,14 +1,12 @@
 sudo tlmgr update --self
 sudo tlmgr install latexmk
-sudo tlmger install 
-curl -o cmu-serif.zip https://fontlibrary.org/assets/downloads/cmu-serif/59c4efc3fa08079ca44060ca808c8f58/cmu-serif.zip
-# Assumes zip file will have a Serif directory 
-open Serif/*.ttf
-rm -rf Serif
-rm -rf cmu-serif.zip
+# curl -o cmu-serif.zip https://fontlibrary.org/assets/downloads/cmu-serif/59c4efc3fa08079ca44060ca808c8f58/cmu-serif.zip
 sudo tlmgr install fontawesome
 sudo tlmgr install titlesec
 sudo tlmgr install enumitem
 curl -o fontawesome.ttf https://raw.githubusercontent.com/creationix/font-awesome/master/FontAwesome.ttf
 open fontawesome.ttf
 rm fontawesome.ttf
+# for mtheme
+sudo tlmgr install pgfopts pgfplots appendixnumberbeamer ctexart
+sudo tlmgr install latexindent
