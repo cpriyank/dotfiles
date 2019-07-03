5881b86c4dc7972bbd019b7961bef39fa3a9bf4d
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2019-07-02 22:20:54 -0700

    do export, set, or source if they don't exist

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,31 +1,26 @@
 ### Environment variables
 # Set EDITOR. Hope I won't have to resort to pico
 if command --search --quiet "nvim"
-    set -gx EDITOR nvim
+	set -gx EDITOR nvim
 else if command --search --quiet "vim"
-    set -gx EDITOR vim
+	set -gx EDITOR vim
 else
-    set -gx EDITOR nano
+	set -gx EDITOR nano
 end
 
 ### Some handy aliases
 source $HOME/.config/fish/aliases.fish
+source $HOME/.config/fish/mpv.fish
 
-### Language specific additions and paths
+# Language specific additions and paths
 if test -e $HOME/.config/fish/go.fish
 	source $HOME/.config/fish/go.fish
 end
 
-### Base16 Shell
-if status --is-interactive
-    eval sh $HOME/.config/base16-shell/scripts/base16-monokai.sh
-end
-
-### Pure prompt theme
-if test -d $HOME/.config/fish/functions/pure
-	set fish_function_path $fish_function_path $HOME/.config/fish/functions/pure
-	source $HOME/.config/fish/functions/pure/fish_prompt.fish
-end
+# Base16 Shell
+# if status --is-interactive; and test -e $HOME/.config/base16-shell/scripts/base16-monokai.sh
+#   eval sh $HOME/.config/base16-shell/scripts/base16-monokai.sh
+# end
 
 ### Color 'less' output
 set -gx LESS_TERMCAP_mb \e'[01;31m'
@@ -40,4 +35,17 @@ set -gx LESS_TERMCAP_us \e'[01;32m'
 # private config not under public source control
 if test -e $HOME/.config/fish/private.fish
 	source $HOME/.config/fish/private.fish
+end
+
+if test -e /usr/local/opt/llvm/bin
+	set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
+end
+
+# THEME PURE #
+if test -e $HOME/.config/fish/functions/theme-pure/functions
+	set fish_function_path /Users/pika/.config/fish/functions/theme-pure/functions/ $fish_function_path
+end
+
+if test -e $HOME/.config/fish/functions/theme-pure/conf.d/pure.fish
+	source $HOME/.config/fish/functions/theme-pure/conf.d/pure.fish
 end
