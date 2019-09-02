0a29d8381f8939bc2caf971a507b4eae1ca383bc
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2019-09-02 13:16:52 -0700

    moved extra things to separate file

---
+++ zsh/.zsh/plugs/zsh_native_enhancement.zsh
@@ -0,0 +1,11 @@
+# Enable fish-like syntax highlighting
+case $(uname) in
+    "Linux") source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ;;
+    "Darwin") source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ;;
+esac
+
+# In OS X autosuggestion config is maintained as a package in brew
+# source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+# In arch, one might have to manage by oneself install zsh-autosuggestions
+[[ -f /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ]] \
+  && source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
