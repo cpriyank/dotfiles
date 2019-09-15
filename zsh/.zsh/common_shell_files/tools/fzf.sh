a9911a5643bce777880f4b3d90dac0a00a4aa149
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2019-09-14 22:48:17 -0700

    use fd as default fzf file provider

---
+++ zsh/.zsh/common_shell_files/tools/fzf.sh
@@ -0,0 +1,14 @@
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+if hash fd 2>/dev/null; then
+  _fzf_compgen_path() {
+    fd --hidden --follow --exclude ".git" . "$1"
+  }
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+fi
