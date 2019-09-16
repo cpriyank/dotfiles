23787ffae54de173a8ad846626b433f88269e63d
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2019-09-16 11:35:20 -0700

    moved to common files dir

--- zsh/.zsh/common_shell_files/common_environment.sh
+++ zsh/.zsh/common_shell_files/common_environment.sh
@@ -1,6 +1,9 @@
 if hash nvim 2>/dev/null
 then
     export EDITOR="nvim"
+elif hash kak 2>/dev/null
+then
+    export EDITOR="kak"
 elif hash vim 2>/dev/null
 then
     export EDITOR="vim"
@@ -10,14 +13,3 @@ fi
 
 # https://wiki.archlinux.org/index.php/locale
 export LC_COLLATE=C
-
-# todo: don't do this by default
-if [[ ! -d $HOME/z/go ]]
-then
-    mkdir -p $HOME/z/go
-fi
-
-export GOPATH=$HOME/z/go
-PATH=$PATH:$GOPATH
-
-PATH=$PATH:$HOME/.gem/ruby/2.6.0/bin
