51f1c8d56919c75d59ad1361dcb6f378b50c7413
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2019-09-16 10:37:21 -0700

    separate files for separate purposes

--- zsh/.zsh/plugs/google-cloud-sdk.zsh
+++ zsh/.zsh/plugs/google-cloud-sdk.zsh
@@ -3,16 +3,3 @@ if [[ -f '/home/prichaud/Downloads/google-cloud-sdk/path.zsh.inc' ]]; then . '/h
 
 # The next line enables shell command completion for gcloud.
 if [[ -f '/home/prichaud/Downloads/google-cloud-sdk/completion.zsh.inc' ]]; then . '/home/prichaud/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
-
-[[ -f ~/.fzf.zsh ]] && source ~/.fzf.zsh
-if command -v colorls 2>&1 >/dev/null && command -v gem 2>&1 >/dev/null; then
-  source $(dirname $(gem which colorls))/tab_complete.sh
-  # override some of existing aliases with colorls
-  source $HOME/.zsh/aliases/colorls.zsh
-fi
-
-source ~/miniconda3/bin/activate
-
-# export DOTNET_ROOT=/opt/dotnet
-# export MSBuildSDKsPath=$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks
-# export PATH=${PATH}:${DOTNET_ROOT}
