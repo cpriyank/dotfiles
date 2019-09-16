e2ce954bbba174daf453b81913b6ed9bf3d0391b
Priyank Chaudhary <priyank.chaudhary90@gmail.com>
2019-09-16 10:29:40 -0700

    comments for ubuntu and debian

--- zsh/.zsh/plugs/zsh_native_enhancement.zsh
+++ zsh/.zsh/plugs/zsh_native_enhancement.zsh
@@ -4,8 +4,11 @@ case $(uname) in
     "Darwin") source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ;;
 esac
 
-# In OS X autosuggestion config is maintained as a package in brew
-# source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+# In OS X, autosuggestion config is maintained as a package in brew
+# source
+[[ -f /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]] \
+	&& source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 # In arch, one might have to manage by oneself install zsh-autosuggestions
 [[ -f /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ]] \
   && source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+# TODO: enable for Debian and Ubuntu
