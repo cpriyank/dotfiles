087b57cd4cca46034cd08a53e165c4cb9f58d8bc
Priyank Chaudhary <5903604+cpriyank@users.noreply.github.com>
2019-11-28 15:57:29 -0800

    lock screen after to force looking outside

--- bin/bin/lookout.zsh
+++ bin/bin/lookout.zsh
@@ -5,13 +5,17 @@ if [[ -s todo ]]
 then
 	var=$(<todo)
 	notify-send -u critical "{${var}}"
+	xsecurelock
 elif [[ -s tips ]]
 then
 	var=$(<tips)
 	notify-send -u critical "{${var}}"
+	xsecurelock
 elif command -v fortune 2>/dev/null
 then
 	notify-send -u critical "`fortune dhammapada`"
+	xsecurelock
 else
-	notify-send -a "Relax" -u critical "Look outside for a while"
+	# notify-send -a "Relax" -u critical "Look outside for a while"
+	xsecurelock
 fi
