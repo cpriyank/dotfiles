28fa6390a9a79f2cd2ff00356d6705074b5c16df
Priyank Chaudhary <5903604+cpriyank@users.noreply.github.com>
2020-05-10 23:10:02 -0700

    give me all branches

--- zsh/.zsh/common_shell_files/functions/functions.sh
+++ zsh/.zsh/common_shell_files/functions/functions.sh
@@ -10,3 +10,11 @@ function md() {
 function f() {
 	find . -name "$1" 2>&1 | grep -v 'Permission denied'
 }
+
+function pullall() {
+	git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
+	git fetch --all
+	git pull --all
+}
+
+alias fetchall="pullall"
