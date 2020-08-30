8169e0f0a296a55b0c11a4a7f21b6a109c54237f
Priyank Chaudhary <5903604+cpriyank@users.noreply.github.com>
2020-08-29 18:13:47 -0700

    log controls and wins

---
+++ fish/.config/fish/functions/loglib.fish
@@ -0,0 +1,15 @@
+function log_lib
+	echo (date) >> ~/Documents/log_lib
+end
+
+function log_sugar
+	echo (date) >> ~/Documents/log_sugar
+end
+
+function log_hungar
+	echo (date) >> ~/Documents/log_hungar
+end
+
+function log_surrender_under_duress
+	echo (date) >> ~/Documents/log_surrender_under_duress
+end
