decc925ce54a4b788ba1f8ec71f69bd71917c36b
Priyank🌸 <priyank@open.gov.sg>
2026-01-21 17:03:08 +0800

    sane defaults

--- init/mac.sh
+++ init/mac.sh
@@ -131,14 +131,14 @@ sudo systemsetup -setcomputersleep Off > /dev/null
 ###############################################################################
 
 # Disable hibernation (speeds up entering sleep mode)
-sudo pmset -a hibernatemode 0
+# sudo pmset -a hibernatemode 0
 
 # Remove the sleep image file to save disk space
-sudo rm /private/var/vm/sleepimage
+# sudo rm /private/var/vm/sleepimage
 # Create a zero-byte file instead…
-sudo touch /private/var/vm/sleepimage
+# sudo touch /private/var/vm/sleepimage
 # …and make sure it can’t be rewritten
-sudo chflags uchg /private/var/vm/sleepimage
+# sudo chflags uchg /private/var/vm/sleepimage
 
 ###############################################################################
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
