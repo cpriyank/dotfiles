b1cd743df5093e3bd77b89845d6d4463bf9fcbe9
Priyank🌸 <p_chaudhary@apple.com>
2026-05-25 15:34:48 +0800

    removed unused

--- zsh/.zsh/plugs/google-cloud-sdk.zsh
+++
@@ -1,7 +0,0 @@
-# The next line updates PATH for the Google Cloud SDK.
-gcloud_sdk_dir="${HOME}/Downloads/google-cloud-sdk"
-if [[ -r "${gcloud_sdk_dir}/path.zsh.inc" ]]; then . "${gcloud_sdk_dir}/path.zsh.inc"; fi
-
-# The next line enables shell command completion for gcloud.
-if [[ -r "${gcloud_sdk_dir}/completion.zsh.inc" ]]; then . "${gcloud_sdk_dir}/completion.zsh.inc"; fi
-unset gcloud_sdk_dir
